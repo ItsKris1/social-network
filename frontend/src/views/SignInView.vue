0172ed6f84dd92af1b8a808adb3358f37c47c774
vic86 <viktorahu@gmail.com>
2022-06-03 16:26:32 +0300

    Starting template

---
+++ frontend/src/views/SignInView.vue
@@ -0,0 +1,19 @@
+<template>
+
+<SignIn />
+    
+</template>
+
+<script>
+
+// @ is an alias to /src
+import SignIn from '@/components/SignIn.vue';
+
+export default {
+    name: 'SignInView',
+    components: {
+        SignIn,
+    }
+}
+
+</script>
