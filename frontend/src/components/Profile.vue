1ab2b53eebebc8c38f722f0b7c29bbb8b6598693
vic86 <viktorahu@gmail.com>
2022-06-12 19:04:50 +0300

    Worked with navbar looking and also profilepage

--- frontend/src/components/Profile.vue
+++ frontend/src/components/Profile.vue
@@ -1,23 +1,57 @@
 <template>
     <div id="profile">
-        <img alt="profilePic">
+        <div>
+            <img id="profileImg" src="../assets/nouserimg.png" alt="profilePic">
+        </div>
+
         <div id="basicInformation">
             <span>James Brown</span>
             <span>e-mail</span>
             <span>birthday date</span>
+            <button id="followBtn" click="follow">Follow</button>
         </div>
-        <button>Subscribe</button>
+        <div id="profileSettings">
+            <span>Privacy change-button</span>
+        </div>
     </div>
-    <div id="profileSettings">
-        <span>Privacy change-button</span>
-    </div>
+
 </template>
 
 <script>
 export default {
-    name: 'Profile'
+    name: 'Profile',
+    methods: {
+        follow() {
+            console.log('subscribe function')
+        }
+    }
 }
 </script>
 
 <style>
+#profile {
+    display: flex;
+    margin-top: 10px;
+}
+
+#profileImg {
+    border-radius: 50%;
+    margin-right: 20px;
+}
+
+#basicInformation {
+    display: grid;
+}
+
+#followBtn {
+    height: 37px;
+    width: 92px;
+    left: 0px;
+    top: 0px;
+    border-radius: 8px;
+    padding: 10px, 15px, 10px, 15px;
+}
+#profileSettings{
+    margin-left: auto;
+}
 </style>
