38294fb03d77d5441434a3a1c99cdf7f6dce4710
CitaZane <zanekrumina2@gmail.com>
2022-06-15 10:15:33 +0300

    logout route and some fixes for sending cookie

--- backend/pkg/handlers/register.go
+++ backend/pkg/handlers/register.go
@@ -1,38 +1,12 @@
 package handlers
 
 import (
-	"fmt"
 	"net/http"
 	"social-network/pkg/models"
 	"social-network/pkg/utils"
 
 	"golang.org/x/crypto/bcrypt"
 )
-
-/* -------------------------------------------------------------------------- */
-/*     test handlers / in the futere should hold login / signup and logout    */
-/* -------------------------------------------------------------------------- */
-/* ----------- not yet connected to front end with right responses ---------- */
-
-// TEST handler for checking if everything connected
-func (handler *Handler) Home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello home"))
-	// Example for db communication
-
-	// newUser := models.User{Name: "TEST", ID: "5"}
-	// err := handler.repos.UserRepo.Add(newUser)
-	// if err != nil {
-	// 	http.Error(w, "Something went wrong", http.StatusBadRequest)
-	// }
-}
-
-// TEST handler for protected route
-func (handler *Handler) Secret(w http.ResponseWriter, r *http.Request) {
-	// access user id
-	userId := r.Context().Value(utils.UserKey)
-	w.Write([]byte("You have access to secret"))
-	fmt.Println(userId, "User validated")
-}
 
 // Register user endpont -> validate inputs / save in db / start session
 func (handler *Handler) Register(w http.ResponseWriter, r *http.Request) {
@@ -85,26 +59,13 @@ func (handler *Handler) Register(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	// Start new session for user (Including cookies)
-	session := utils.SessionStart(w, r, userID)
-	// Save session in database
-	errSession := handler.repos.SessionRepo.Set(session)
-	if errSession != nil {
-		utils.RespondWithError(w, "Error on creating new session", 500)
-	}
-}
-
-// TEST  handler for logout
-func (handler *Handler) Logout(w http.ResponseWriter, r *http.Request) {
-	// access user id
-	userId := r.Context().Value(utils.UserKey).(string)
-	// delete session
-	session := models.Session{UserID: userId}
-	errSession := handler.repos.SessionRepo.Delete(session)
-	if errSession != nil {
-		fmt.Println("error on deleting session", errSession)
-		return
-	}
-	// delete cookie
-	utils.DeleteCookie(w)
-	w.Write([]byte("Logged out"))
+	/* ---------- code commented out if from register redirect to login --------- */
+	/*
+		session := utils.SessionStart(w, r, userID)
+		// Save session in database
+		errSession := handler.repos.SessionRepo.Set(session)
+		if errSession != nil {
+			utils.RespondWithError(w, "Error on creating new session", 500)
+		}
+	*/
 }
--- checklist.md
+++ checklist.md
@@ -2,27 +2,27 @@
 
 #### server.go
 
-- [ ] Replace test routes
+- [x] Replace test routes
 
 #### pkg/db
 
 - [ ] sqlite.go - Finalize InitDB() function (db name)
 - [ ] sqlite.go - Add function for creating tables if not exist
 - [x] users.go - Delete Test function
-- [ ] sqlite.go - delete temporery database function
+- [ ] sqlite.go - delete temporary database function
 
 #### pkg/handlers
 
 - [x] Middleware - check functionality after real request from front-end
-- [ ] Middleware - Handle errors and responses
-- [ ] register.go - delete test handlers
+- [x] Middleware - Handle errors and responses
+- [x] register.go - delete test handlers
 - [x] register.go - handle errors and responses
 - [x] user.go - delete temp function - Add(User)error
 
 #### pkg/utils
 
 - [x] session.go - check if cookie gets sent to front end
-- [ ] session.go - check if cookie gets destroyed when conn to front end
+- [x] session.go - check if cookie gets destroyed when conn to front end
 
 ### Front-end
 
