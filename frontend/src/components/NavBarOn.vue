d4666d16ff52da5f5cfa02e248ed73a7c9d0003a
vic86 <viktorahu@gmail.com>
2022-06-18 23:58:25 +0300

    header styling

--- frontend/src/components/NavBarOn.vue
+++ frontend/src/components/NavBarOn.vue
@@ -5,15 +5,18 @@
             <a href="/" id="logo">Social Network</a>
         </div>
         <Search />
-        <!-- <router-link to="/"><img src="../assets/home.png" alt="home.png"><span>Home</span></router-link> -->
         <Notifications />
         <div id="a">
             <router-link to="/profile">
-                <img id="navbarUserPic" :src="'http://localhost:8081/' + user.avatar" alt="profilePic">
+                <!-- <img id="navbarUserPic" :src="'http://localhost:8081/' + user.avatar" alt="profilePic"> -->
+
+                <!-- <span id="navBarName">{{ user.nickname }}</span> -->
+                <span id="navBarName">My profile</span>
+
             </router-link>
-            <span id="navBarName">{{ user.nickname }}</span>
         </div>
-        <img @click="logout" id="logoutBtn" src="../assets/logout.png">
+        <!-- <img @click="logout" id="logoutBtn" src="../assets/logout.png"> -->
+        <span @click="logout" id="logoutBtn">Log out</span>
     </div>
 
 </template>
@@ -65,15 +68,23 @@ export default {
 </script>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200&family=Water+Brush&display=swap');
+
 #navbar {
     display: flex;
     justify-content: space-around;
     margin-top: 10px;
     align-items: center;
+
+    background: #4B5283;
+    font-family: 'Poppins';
+    color: #EEEEEE;
+    
 }
 
 #logo {
-    font-family: Inter;
+    /* font-family: Inter; */
+    
     font-size: 24px;
     font-weight: 649;
     line-height: 29px;
@@ -81,11 +92,12 @@ export default {
     text-align: center;
     font-variation-settings: 'slnt' 0;
     text-decoration: none;
+    color: #EEEEEE;
 }
 
 #logoutBtn {
-    width: 16px;
-    height: 20px;
+    /* width: 16px;
+    height: 20px; */
     cursor: pointer;
 }
 
@@ -100,4 +112,24 @@ export default {
     display: flex;
     align-items: center;
 }
+
+#navBarName {
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 146.02%;
+    /* or 23px */
+
+    display: flex;
+    align-items: center;
+    text-align: center;
+    text-decoration: none;
+
+    color: #E4E3E3;
+}
+a:link { text-decoration: none; }
+a:visited { text-decoration: none; }
+a:hover { text-decoration: none; }
+a:active { text-decoration: none; }
 </style>
