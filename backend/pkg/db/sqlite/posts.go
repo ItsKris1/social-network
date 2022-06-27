790b32f8ae9f8f50b32aeed1b8696056296646e1
CitaZane <zanekrumina2@gmail.com>
2022-06-27 10:34:38 +0300

    removed group posts from main and profile queries

--- backend/pkg/db/sqlite/posts.go
+++ backend/pkg/db/sqlite/posts.go
@@ -17,7 +17,7 @@ type PostRepository struct {
 // all posts if user is an author
 func (repo *PostRepository) GetAll(userID string) ([]models.Post, error) {
 	var posts []models.Post
-	rows, err := repo.DB.Query("SELECT post_id , created_by, content, image  FROM posts WHERE (group_id NOT NULL AND (SELECT COUNT() FROM group_users WHERE posts.group_id = group_users.group_id AND '"+userID+"' = group_users.user_id)=1) OR (group_id IS NULL  AND visibility = 'PUBLIC') OR (group_id IS NULL AND visibility = 'PRIVATE' AND (SELECT COUNT() FROM followers WHERE posts.created_by = followers.user_id AND followers.follower_id = '"+userID+"') = 1 ) OR (group_id IS NULL  AND visibility = 'ALMOST_PRIVATE' AND (SELECT COUNT() FROM almost_private WHERE almost_private.post_id = posts.post_id AND almost_private.user_id = '"+userID+"')=1) OR created_by = '"+userID+"' ORDER BY created_at DESC;", userID)
+	rows, err := repo.DB.Query("SELECT post_id , created_by, content, image  FROM posts WHERE (group_id IS NULL  AND visibility = 'PUBLIC') OR (group_id IS NULL AND visibility = 'PRIVATE' AND (SELECT COUNT() FROM followers WHERE posts.created_by = followers.user_id AND followers.follower_id = '"+userID+"') = 1 ) OR (group_id IS NULL  AND visibility = 'ALMOST_PRIVATE' AND (SELECT COUNT() FROM almost_private WHERE almost_private.post_id = posts.post_id AND almost_private.user_id = '"+userID+"')=1) OR  (group_id IS NULL  AND created_by = '"+userID+"' )ORDER BY created_at DESC;", userID)
 	if err != nil {
 		return posts, err
 	}
@@ -33,7 +33,7 @@ func (repo *PostRepository) GetUserPosts(userID, currentUserID string) ([]models
 	// get all posts that do not belong to group
 	// get group posts only if current user alsa a member or admin
 	var posts []models.Post
-	rows, err := repo.DB.Query("SELECT post_id , created_by, content, image  FROM posts WHERE (group_id NOT NULL AND (SELECT COUNT() FROM group_users WHERE (posts.group_id = group_users.group_id AND '" + userID + "' = group_users.user_id) AND (posts.group_id = group_users.group_id AND '" + currentUserID + "' = group_users.user_id))=2) OR (group_id IS NULL  AND visibility = 'PUBLIC' AND created_by = '" + userID + "') OR (group_id IS NULL AND visibility = 'PRIVATE' AND created_by = '" + userID + "' AND (SELECT COUNT() FROM followers WHERE posts.created_by = followers.user_id AND followers.follower_id = '" + currentUserID + "') = 1 ) OR (group_id IS NULL  AND visibility = 'ALMOST_PRIVATE' AND created_by = '" + userID + "' AND (SELECT COUNT() FROM almost_private WHERE almost_private.post_id = posts.post_id AND almost_private.user_id = '" + currentUserID + "')=1) OR (created_by = '" + userID + "' AND '" + userID + "' = '" + currentUserID + "') ORDER BY created_at DESC;")
+	rows, err := repo.DB.Query("SELECT post_id , created_by, content, image  FROM posts WHERE (group_id IS NULL  AND visibility = 'PUBLIC' AND created_by = '" + userID + "') OR (group_id IS NULL AND visibility = 'PRIVATE' AND created_by = '" + userID + "' AND (SELECT COUNT() FROM followers WHERE posts.created_by = followers.user_id AND followers.follower_id = '" + currentUserID + "') = 1 ) OR (group_id IS NULL  AND visibility = 'ALMOST_PRIVATE' AND created_by = '" + userID + "' AND (SELECT COUNT() FROM almost_private WHERE almost_private.post_id = posts.post_id AND almost_private.user_id = '" + currentUserID + "')=1) OR (group_id IS NULL  AND created_by = '" + userID + "' AND '" + userID + "' = '" + currentUserID + "') ORDER BY created_at DESC;")
 	if err != nil {
 		return posts, err
 	}
