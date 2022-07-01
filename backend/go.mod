a5ef2dba10d48b526a88cfcf44b2ba6f89a8a7bc
itskris <kkangro7@gmail.com>
2022-07-01 12:21:17 +0300

    css

--- backend/go.mod
+++ backend/go.mod
@@ -5,6 +5,6 @@ go 1.18
 require github.com/mattn/go-sqlite3 v1.14.13
 
 require (
-	github.com/satori/go.uuid v1.2.0 // indirect
-	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e // indirect
+	github.com/satori/go.uuid v1.2.0
+	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e
 )
--- frontend/src/views/MainView.vue
+++ frontend/src/views/MainView.vue
@@ -1,61 +1,10 @@
 <template>
     <NavBarOn />
-    <NewPost />
+    <!-- <NewPost /> -->
     <AllPosts />
-<<<<<<< HEAD
 
 
-    <div class="item-list">
-        <h3>Title</h3>
-        <ul class="item-list__items">
-            <li class="item-list__item">
-                <div class="user-picture small"></div>
-                <div class="item-list__text">Item 1</div>
-            </li>
-            <li class="item-list__item">
-                <div class="user-picture small"></div>
-                <div class="item-list__text">Item 2</div>
-            </li>
-
-            <li class="item-list__item">
-                <div class="user-picture small"></div>
-                <div class="item-list__text">Item 3</div>
-            </li>
-
-            <li class="item-list__item">
-                <div class="user-picture small"></div>
-                <div class="item-list__text">Item 4</div>
-            </li>
-        </ul>
-        <button class="btn">Button<i class="uil uil-arrow-circle-right "></i></button>
-    </div>
-
-    <div class="item-list">
-        <h3>Title</h3>
-        <ul class="item-list__items">
-            <li class="item-list__item">
-                <img class="small" src="../assets/arrow-circle-right.svg">
-                <div class="item-list__text">Item 1</div>
-            </li>
-            <li class="item-list__item">
-                <img class="small" src="../assets/arrow-circle-right.svg">
-                <div class="item-list__text">Item 2</div>
-            </li>
-            <li class="item-list__item">
-                <img class="small" src="../assets/arrow-circle-right.svg">
-                <div class="item-list__text">Item 3</div>
-            </li>
-            <li class="item-list__item">
-                <img class="small" src="../assets/arrow-circle-right.svg">
-                <div class="item-list__text">Item 4</div>
-            </li>
-
-        </ul>
-        <button class="btn">Button<i class="uil uil-arrow-circle-right"></i></button>
-    </div>
-=======
     <Groups />
->>>>>>> origin/vue
 </template>
 
 <script>
@@ -66,43 +15,9 @@ import Groups from '@/components/Groups.vue'
 
 export default {
     name: 'MainView',
-<<<<<<< HEAD
-    components: { NavBarOn, AllPosts },
-=======
     components: { NavBarOn, NewPost, AllPosts, Groups },
->>>>>>> origin/vue
 }
 </script>
 
 <style>
-.item-list {
-    display: flex;
-    flex-direction: column;
-    gap: 25px;
-    padding: 25px 30px 25px 30px;
-    background-color: var(--color-white);
-    width: max-content;
-    max-width: 250px;
-    box-shadow: var(--container-shadow);
-    border-radius: 10px;
-
-}
-
-
-.item-list__item {
-    display: flex;
-    gap: 7.5px;
-    align-items: center;
-
-}
-
-.item-list__item:nth-child(1)~* {
-    margin-top: 15px;
-}
-
-.item-list__text {
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-}
 </style>
