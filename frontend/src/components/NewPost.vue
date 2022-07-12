4738109d545bc7b72636cf8e1d68d1b0cba01b73
itskris <kkangro7@gmail.com>
2022-07-12 18:41:43 +0300

    new post form -> clear input, close modal

--- frontend/src/components/NewPost.vue
+++ frontend/src/components/NewPost.vue
@@ -6,11 +6,6 @@
         <i class="uil uil-edit"></i>
     </button>
 
-
-    <!-- <div id="postBtn" @click="toggle">
-        <span>Start a post</span>
-        <img src="../assets/writepost.png" alt="">
-    </div> -->
     <div class="modal" v-show="isOpen">
 
         <div class="modal-content">
@@ -24,8 +19,8 @@
                     <div class="select-wrapper">
                         <img src="../assets/icons/angle-down.svg" alt="" srcset="">
 
-                        <select v-model="newpost.privacy" name="post_privacy" @change="getFollowers" id="post_privacy"
-                            required>
+                        <select v-model="newpost.privacy" @change="getFollowers" id="post_privacy"
+                                required>
                             <option value="" selected disabled hidden>Choose here</option>
                             <option value="public" selected>Everyone</option>
                             <option value="private">Followers</option>
@@ -38,7 +33,7 @@
 
                         <div v-for="follower in fetchedFollowers.followers">
                             <input type="checkbox" :id="follower" name="privacy" :value="follower"
-                                v-model="checkedFollowers" />
+                                   v-model="checkedFollowers" />
                             <label :for="follower">{{ follower }}</label>
                         </div>
                         <div>Checked names: {{ checkedFollowers }}</div>
@@ -49,7 +44,7 @@
                 <div class="form-input">
                     <label for="description">Description</label>
                     <textarea id="description" name="description" rows="4" cols="50" v-model="newpost.body"
-                        placeholder="What are you thinking?"></textarea>
+                              placeholder="What are you thinking?" required></textarea>
                 </div>
 
                 <div class="btns-wrapper">
@@ -58,7 +53,7 @@
                         <img src="../assets/addimg.png" />
                     </label>
                     <input id="upload-image" @change="checkPicture" type="file"
-                        accept="image/png, image/gif, image/jpeg" />
+                           accept="image/png, image/gif, image/jpeg" />
 
                     <button class="btn" type="submit">Post</button>
 
@@ -66,65 +61,6 @@
             </form>
         </div>
     </div>
-
-
-
-
-
-    <!-- 
-    <div class="modal">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h2>Modal title</h2>
-                <i class="uil uil-times close"></i>
-            </div>
-
-            <form>
-                <div class="form-input">
-                    <label for="username">First name</label>
-                    <input type="text" name="username" id="username">
-                </div>
-
-                <div class="form-input">
-                    <label for="username">Last name</label>
-                    <input type="text" name="username" id="username">
-                </div>
-
-                <div class="form-input">
-                    <label for="username">Email</label>
-                    <input type="text" name="username" id="username">
-                </div>
-
-                <div class="form-input">
-                    <label for="date">Select a date</label>
-                    <input type="date" name="date" id="date">
-                </div>
-
-                <div class="form-input">
-                    <label for="description">Description</label>
-                    <textarea id="description" name="description" rows="4" cols="50"></textarea>
-                </div>
-
-                <div class="form-input">
-                    <label for="cars">Select an option</label>
-
-                    <select name="cars" id="cars">
-                        <option value="" selected disabled hidden>Choose here</option>
-                        <option value="volvo">Volvo</option>
-                        <option value="saab">Saab</option>
-                        <option value="mercedes">Mercedes</option>
-                        <option value="audi">Audi</option>
-                    </select>
-                </div>
-            </form>
-
-            <button class="btn">Button</button>
-
-        </div>
-    </div> -->
-
-
-
 
 </template>
 
@@ -146,6 +82,9 @@ export default {
     },
     methods: {
         toggle() {
+            this.newpost.privacy = "";
+            this.newpost.body = "";
+            this.newpost.image = "";
             this.isOpen = !this.isOpen
         },
 
@@ -202,6 +141,9 @@ export default {
             })
             this.$store.dispatch('fetchPosts')
             console.log('Post submitted');
+
+
+            this.toggle();
         },
     },
 }
