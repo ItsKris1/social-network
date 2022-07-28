cf5aece9da61edfb64192620d361889658cee74e
vic86 <viktorahu@gmail.com>
2022-07-28 19:47:35 +0300

    Added New Event functionality.

--- frontend/src/components/GroupEvents.vue
+++ frontend/src/components/GroupEvents.vue
@@ -10,11 +10,23 @@
         <button class="btn form-submit" @click="toggleModal">New event</button>
 
         <Modal v-if="this.isOpen" @closeModal="toggleModal">
-            <!-- <template #title>Choose users:</template>
+            <template #title>Crete new event</template>
             <template #body>
-                
-                <button class="btn form-submit" @click="toggleModal() ; inviteUsersToGroup()">Invite</button>
-            </template> -->
+                <span>Title</span>
+                <input type="text" v-model="this.formData.title">
+                <span>Description</span>
+                <textarea v-model="this.formData.content" cols="30" rows="3"
+                    placeholder="What is this about?"></textarea>
+                <span>Date</span>
+                <input v-model="this.formData.date" type="date">
+                <span>Going</span>
+                <select v-model="this.formData.going" name="going">
+                    <option value="YES">Yes</option>
+                    <option value="NO">No</option>
+                </select>
+                <button class="btn form-submit" @click="toggleModal() ; createNewEvent()">Create</button>
+            </template>
+
         </Modal>
     </div>
 </template>
@@ -27,7 +39,13 @@ export default {
     data() {
         return {
             groupEvents: ["a", "b", "c"],
-            isOpen:false
+            isOpen: false,
+            formData: {
+                title: "",
+                content: "",
+                date: null,
+                going: null
+            }
         };
     },
     created() {
@@ -40,9 +58,28 @@ export default {
             })
                 .then((response => response.json()))
                 .then((json => {
-                console.log("All Events:", json);
-                // this.GroupEvents = json.users;
-            }));
+                    console.log("All Events:", json);
+                    this.groupEvents = json.events
+                }));
+        },
+        async createNewEvent() {
+
+            await fetch('http://localhost:8081/newEvent', {
+                method: 'POST',
+                credentials: 'include',
+                body: JSON.stringify({
+                    groupID: this.$route.params.id,
+                    title: this.formData.title,
+                    content: this.formData.content,
+                    date: this.formData.date,
+                    going: this.formData.going
+                })
+            })
+                .then((r => r.json()))
+                .then((json => {
+                    console.log("newEvent", json);
+                }))
+                this.getGroupEvents()
         },
         toggleModal() {
             this.isOpen = !this.isOpen;
